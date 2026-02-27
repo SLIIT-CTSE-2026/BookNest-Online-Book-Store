7ed4aac001ac2a6d723531917ceb8a9692fc04f0
Dinuka Rathnayake <it22140616@my.sliit.lk>
2026-02-27 08:13:01 +0530

    Created initial vite frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <div>
+        <p>
+          Click on the Vite and React logos to learn more
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
