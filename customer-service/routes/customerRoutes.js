98e3af17940c7fe1472337b73d819b03e6486611
Dinuka Rathnayake <174526798+sdinukarathnayake@users.noreply.github.com>
2026-03-21 19:15:18 +0530

    Customer routes update

--- customer-service/routes/customerRoutes.js
+++ customer-service/routes/customerRoutes.js
@@ -2,12 +2,10 @@ import express from 'express';
 import { getAllCustomers, getCustomerById, updateCustomer, deleteCustomer, createCustomerProfile } from '../controllers/customerController.js';
 const router = express.Router();
 
-router.post('/customers', createCustomerProfile);
-router.post('/profile', createCustomerProfile);
-
 router.get('/', getAllCustomers);
 router.get('/:customerId', getCustomerById);
 router.put('/:customerId', updateCustomer);
 router.delete('/:customerId', deleteCustomer);
+router.post('/profile', createCustomerProfile);
 
 export default router;
