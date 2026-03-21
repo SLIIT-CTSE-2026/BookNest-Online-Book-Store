0deaff9c8809878abf14583745d60c3a092fd712
Dinuka Rathnayake <174526798+sdinukarathnayake@users.noreply.github.com>
2026-03-21 19:40:04 +0530

    Updated docker files

--- auth-service/server.js
+++ auth-service/server.js
@@ -19,8 +19,8 @@ const start = async () => {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("Auth Service - Database Connected");
 
-    app.listen(process.env.PORT || 5001, () => {
-      console.log(`Auth Service running on port ${process.env.PORT || 5001}`);
+    app.listen(process.env.PORT, () => {
+      console.log(`Auth Service running on port ${process.env.PORT}`);
     });
   } catch (error) {
     console.error("Auth Service - Initialization error:", error.message);
